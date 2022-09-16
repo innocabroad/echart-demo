dac20799dad2d861493308311399610e5ccc8f24
innocabroad <innoc_abroad@163.com>
2022-09-16 18:48:14 +0800

    Add simple code

--- index.js
+++ index.js
@@ -1,106 +1,75 @@
 import * as echarts from 'echarts';
 
-type EChartsOption = echarts.EChartsOption
-
-var chartDom = document.getElementById('main')!;
+var chartDom = document.getElementById('test2');
 var myChart = echarts.init(chartDom);
-var option: EChartsOption;
+var option;
 
+const data = genData(50);
 option = {
   title: {
-    text: 'Stacked Area Chart'
+    text: '同名数量统计',
+    subtext: '纯属虚构',
+    left: 'center'
   },
   tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross',
-      label: {
-        backgroundColor: '#6a7985'
-      }
-    }
+    trigger: 'item',
+    formatter: '{a} <br/>{b} : {c} ({d}%)'
   },
   legend: {
-    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+    type: 'scroll',
+    orient: 'vertical',
+    right: 10,
+    top: 20,
+    bottom: 20,
+    data: data.legendData
   },
-  toolbox: {
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  xAxis: [
-    {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value'
-    }
-  ],
   series: [
     {
-      name: 'Email',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
+      name: '姓名',
+      type: 'pie',
+      radius: '55%',
+      center: ['40%', '50%'],
+      data: data.seriesData,
       emphasis: {
-        focus: 'series'
-      },
-      data: [120, 132, 101, 134, 90, 230, 210]
-    },
-    {
-      name: 'Union Ads',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [220, 182, 191, 234, 290, 330, 310]
-    },
-    {
-      name: 'Video Ads',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [150, 232, 201, 154, 190, 330, 410]
-    },
-    {
-      name: 'Direct',
-      type: 'line',
-      stack: 'Total',
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [320, 332, 301, 334, 390, 330, 320]
-    },
-    {
-      name: 'Search Engine',
-      type: 'line',
-      stack: 'Total',
-      label: {
-        show: true,
-        position: 'top'
-      },
-      areaStyle: {},
-      emphasis: {
-        focus: 'series'
-      },
-      data: [820, 932, 901, 934, 1290, 1330, 1320]
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
     }
   ]
 };
+function genData(count) {
+  // prettier-ignore
+  const nameList = [
+        '赵', '钱', '孙', '李', '周', '吴', '郑', '王', '冯', '陈', '褚', '卫', '蒋', '沈', '韩', '杨', '朱', '秦', '尤', '许', '何', '吕', '施', '张', '孔', '曹', '严', '华', '金', '魏', '陶', '姜', '戚', '谢', '邹', '喻', '柏', '水', '窦', '章', '云', '苏', '潘', '葛', '奚', '范', '彭', '郎', '鲁', '韦', '昌', '马', '苗', '凤', '花', '方', '俞', '任', '袁', '柳', '酆', '鲍', '史', '唐', '费', '廉', '岑', '薛', '雷', '贺', '倪', '汤', '滕', '殷', '罗', '毕', '郝', '邬', '安', '常', '乐', '于', '时', '傅', '皮', '卞', '齐', '康', '伍', '余', '元', '卜', '顾', '孟', '平', '黄', '和', '穆', '萧', '尹', '姚', '邵', '湛', '汪', '祁', '毛', '禹', '狄', '米', '贝', '明', '臧', '计', '伏', '成', '戴', '谈', '宋', '茅', '庞', '熊', '纪', '舒', '屈', '项', '祝', '董', '梁', '杜', '阮', '蓝', '闵', '席', '季', '麻', '强', '贾', '路', '娄', '危'
+    ];
+  const legendData = [];
+  const seriesData = [];
+  for (var i = 0; i < count; i++) {
+    var name =
+      Math.random() > 0.65
+        ? makeWord(4, 1) + '·' + makeWord(3, 0)
+        : makeWord(2, 1);
+    legendData.push(name);
+    seriesData.push({
+      name: name,
+      value: Math.round(Math.random() * 100000)
+    });
+  }
+  return {
+    legendData: legendData,
+    seriesData: seriesData
+  };
+  function makeWord(max, min) {
+    const nameLen = Math.ceil(Math.random() * max + min);
+    const name = [];
+    for (var i = 0; i < nameLen; i++) {
+      name.push(nameList[Math.round(Math.random() * nameList.length - 1)]);
+    }
+    return name.join('');
+  }
+}
 
 option && myChart.setOption(option);
